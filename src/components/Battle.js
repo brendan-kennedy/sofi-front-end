f7fdea277e00edd84b82388fa92190eb9bde5e13
James Haggerty <jpm.haggerty@gmail.com>
2021-11-05 11:25:01 -0400

    We are the champions

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
@@ -9,23 +9,50 @@ import TempDrawerDuel from "../components/TempDrawerDuel.js";
 import "../App.css";
 
 function Battle({ match, matchfull, handleChipClick }) {
-  console.log("Match: ", match);
+
+  useEffect(() => {
+    fetchResults();
+  }, []);
+
+  const [results, setResults] = useState([]);
+  const [randomChar, setRandomChar] = useState({});
+
+  const fetchResults = async () => {
+    const data = await fetch("http://localhost:3001/GOT/characters");
+
+    const results = await data.json();
+    console.log(results.characters);
+    setResults(results.characters);
+  };
+
+
+  const handleClick = () => {
+    console.log("Clicker");
+    let randomNumber = Math.floor(Math.random() * (results.length - 1));
+    console.log("Random: ", randomNumber);
+    let charSelectedByRandomNumber = results.find(
+      (elem, index) => index === randomNumber
+    );
+    setRandomChar(charSelectedByRandomNumber);
+    console.log(charSelectedByRandomNumber);
+  };
+
+
   return (
     <section>
       <main>
         <Box>
+              {/* USER CARD */}
+              <Card sx={{ maxWidth: 345 }}>
+                {console.log("Champion: ", match[0])}
+                <CardHeader title= {match[0] ? match[0].name : ""} />
+                {/* {match[0] ? match[0].order : ""} */}
+                <CardMedia component="img" height="194" image={match[0] ? match[0].image : ""} alt="Logo" />
+              </Card>
 
-          {/* USER CARD */}
-          <Card sx={{ maxWidth: 345 }}>
-            {console.log("Match: ", match[0].name)}
-            <CardHeader title= {match[0] ? match[0].name : ""} />
-            {/* {match[0] ? match[0].order : ""} */}
-            <CardMedia component="img" height="194" image={match[0] ? match[0].image : ""} alt="Logo" />
-          </Card>
-
-          <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <TempDrawerDuel match={matchfull} handleChipClick={handleChipClick} />
-          </Box>
+              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                <TempDrawerDuel match={matchfull} handleChipClick={handleChipClick} />
+              </Box>
         </Box>
 
         <Box>
@@ -33,17 +60,26 @@ function Battle({ match, matchfull, handleChipClick }) {
         </Box>
 
         <Box>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader title="JAMIE LANISTER" />
-            <CardMedia component="img" height="194" image="url" alt="Logo" />
-          </Card>
-          <Button variant="contained">Select Your Opponent</Button>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardHeader title={randomChar?.name} />
+              <CardMedia
+                component="img"
+                height="194"
+                image= {randomChar?.image}
+                alt={`This character has no image. Sad ${randomChar?.name || 'character'} :(`}
+              />
+            </Card>
+
+            <Button variant="contained" onClick={() => handleClick()}>
+              Select Your Opponent
+            </Button>
         </Box>
+
       </main>
 
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <h1>JON SNOW DECLARES VICTORY!</h1>
-      </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <h1>JON SNOW DECLARES VICTORY!</h1>
+        </Box>
     </section>
   );
 }
